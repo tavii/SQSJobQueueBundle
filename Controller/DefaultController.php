4a6074a7dafa38b2482a8a5fac66a32fc8c32fd7
polidog <polidogs@gmail.com>
2016-03-14 22:35:38 +0900

    管理画面の追加

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,8 +10,14 @@ namespace Tavii\SQSJobQueueBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Tavii\SQSJobQueueBundle\Storage\DoctrineStorage;
 
+/**
+ * Class DefaultController
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         /** @var DoctrineStorage $storage */
@@ -23,5 +29,24 @@ class DefaultController extends Controller
         return $this->render('TaviiSQSJobQueueBundle:Default:index.html.twig', [
             'workers' => $workers
         ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function sqslistAction()
+    {
+        $prefix = $this->getParameter('sqs_job_queue.prefix');
+        $client = $this->get('sqs_job_queue.client');
+        $results = $client->listQueues(array(
+            'QueueNamePrefix' => $prefix,
+        ));
+
+        return $this->render('TaviiSQSJobQueueBundle:Default:sqslist.html.twig', [
+            'queueUrls' => $results['QueueUrls'],
+            'prefix' => $prefix
+        ]);
+
+
     }
 }
